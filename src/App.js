412013ef099cd4556bef8f65a9b9df4c166fe541
JideonwoFelix <99179966+JideonwoFelix@users.noreply.github.com>
2023-06-18 18:13:36 +0100

    m-

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Coming_soon from './Pages/coming_soon';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Coming_soon/>
     </div>
   );
 }
---
+++ src/Pages/coming_soon.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Coming_soon() {
+  return (
+    <>
+        <div className="xbg-green-300 font-bold text-9xl">
+            Coming Soon
+        </div>
+        <div className='text-gray-700 text-3xl mt-20'>
+            Still In Production
+        </div>
+    </>
+  )
+}
